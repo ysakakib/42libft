18a604229a1019703568ce4c63a1d8fbfa3f022d
Yu Sakakibara <yusakaki@c1r2s6.42tokyo.jp>
2026-04-25 14:17:48 +0900

    a

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,10 +6,17 @@
 /*   By: yusakaki <yusakaki@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/24 16:50:23 by yusakaki          #+#    #+#             */
-/*   Updated: 2026/04/24 16:53:45 by yusakaki         ###   ########.fr       */
+/*   Updated: 2026/04/25 14:14:10 by yusakaki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void *ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
+void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
+{
+}
+
+int	main(void)
+{
+
+}
