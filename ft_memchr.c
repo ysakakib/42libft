cc91c5b7e2ef2e18627ddbbb6922457b4cd35144
Yu Sakakibara <ict25095@stu.cbc.ac.jp>
2026-05-01 20:01:11 +0900

    A

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: yusakaki <yusakaki@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/28 10:54:49 by yusakaki          #+#    #+#             */
-/*   Updated: 2026/04/29 22:24:25 by yusakaki         ###   ########.fr       */
+/*   Updated: 2026/05/01 18:39:46 by yusakaki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,35 +14,47 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	unsigned char	*str;
-	unsigned char	uc;
-	size_t			i;
-
-	str = (unsigned char *)s;
-	uc = (unsigned char)c;
-	i = 0;
+	size_t i = 0;
+	unsigned char *str = (unsigned chqar *)s;
 	while (i < n)
 	{
-		if (str[i] == uc)
-			return ((void *)&str[i]);
+		if (str[i] == c)
+			return (str[i]);
 		i++;
 	}
-	return (NULL);
 }
 
-// int main(void)
+int main(void)
+{
+	char *s = "hello world";
+	char c = 'e';
+	printf("%p\n", ft_memchr(s, c, 6));
+	s = "hello world";
+	c = 'e';
+	printf("%p\n", memchr(s, c, 6));
+	s = "hello world";
+	c = 'a';
+	printf("%p\n", ft_memchr(s, c, 6));
+	s = "hello world";
+	c = 'a';
+	printf("%p\n", memchr(s, c, 6));
+	return (0);
+}
+
+// void	*ft_memchr(const void *s, int c, size_t n)
 // {
-// 	char *s = "hello world";
-// 	char c = 'e';
-// 	printf("%p\n", ft_memchr(s, c, 6));
-// 	s = "hello world";
-// 	c = 'e';
-// 	printf("%p\n", memchr(s, c, 6));
-// 	s = "hello world";
-// 	c = 'a';
-// 	printf("%p\n", ft_memchr(s, c, 6));
-// 	s = "hello world";
-// 	c = 'a';
-// 	printf("%p\n", memchr(s, c, 6));
-// 	return (0);
+// 	unsigned char	*str;
+// 	unsigned char	uc;
+// 	size_t			i;
+
+// 	str = (unsigned char *)s;
+// 	uc = (unsigned char)c;
+// 	i = 0;
+// 	while (i < n)
+// 	{
+// 		if (str[i] == uc)
+// 			return ((void *)&str[i]);
+// 		i++;
+// 	}
+// 	return (NULL);
 // }
