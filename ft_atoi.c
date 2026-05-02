02bf5fdcae81d573b539c39e17e1bfcdc40b2d47
Yu Sakakibara <yusakaki@c2r3s1.42tokyo.jp>
2026-05-02 16:46:05 +0900

    atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,36 +6,36 @@
 /*   By: yusakaki <yusakaki@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/28 16:00:16 by yusakaki          #+#    #+#             */
-/*   Updated: 2026/05/01 08:59:35 by yusakaki         ###   ########.fr       */
+/*   Updated: 2026/05/01 22:22:57 by yusakaki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-// int	ft_atoi(const char *str)
-// {
-// 	int		i;
-// 	int		sign;
-// 	long	result;
+int	ft_atoi(const char *str)
+{
+	int		i;
+	int		sign;
+	long	result;
 
-// 	i = 0;
-// 	sign = 1;
-// 	result = 0;
-// 	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
-// 		i++;
-// 	if (str[i] == '-' || str[i] == '+')
-// 	{
-// 		if (str[i] == '-')
-// 			sign = -1;
-// 		i++;
-// 	}
-// 	while (str[i] >= '0' && str[i] <= '9')
-// 	{
-// 		result = result * 10 + (str[i] - '0');
-// 		i++;
-// 	}
-// 	return ((int)(sign * result));
-// }
+	i = 0;
+	sign = 1;
+	result = 0;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + (str[i] - '0');
+		i++;
+	}
+	return ((int)(sign * result));
+}
 
 // int main(void)
 // {
