5c364d7c878aa0ea64c4c1564cee1cc578875306
Yu Sakakibara <yusakaki@c2r1s1.42tokyo.jp>
2026-05-03 12:03:14 +0900

    A

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,32 +6,30 @@
 /*   By: yusakaki <yusakaki@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/24 16:52:17 by yusakaki          #+#    #+#             */
-/*   Updated: 2026/04/28 12:14:31 by yusakaki         ###   ########.fr       */
+/*   Updated: 2026/05/03 03:28:39 by yusakaki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memmove(void *dst, const void *src, size_t len)
+void	*ft_memmove(void *dst, const void *src, size_t n)
 {
-	unsigned char		*d;
-	const unsigned char	*s;
+	unsigned char	*d;
+	unsigned char	*s;
 
-	// if (!dst && !src)
-	// 	return (NULL);
 	d = (unsigned char *)dst;
-	s = (const unsigned char *)src;
+	s = (unsigned char *)src;
 	if (d < s)
-		return (ft_memcpy(dst, src, len));
+		return (memcpy(dst, src, n));
 	else
 	{
-		while (len > 0)
+		while (n > 0)
 		{
-			len--;
-			d[len] = s[len];
+			n--;
+			d[n] = s[n];
 		}
 	}
-	return (d);
+	return (dst);
 }
 
 // int	main(void)
@@ -39,9 +37,49 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 // 	char	str1[20] = "hello world";
 // 	char	str2[20] = "hello world";
 
-// 	memmove(str1 + 2, str1, 3);
-// 	printf("%s\n", str1);
-// 	ft_memmove(str2 + 2, str2, 3);
-// 	printf("%s\n", str2);
+// 	printf("%s\n", (char *)memmove(str1 + 2, str1, 3));
+// 	printf("%s\n", (char *)ft_memmove(str2 + 2, str2, 3));
 // 	return (0);
+// }
+
+// void	*ft_memmove(void *dst, const void *src, size_t len)
+// {
+// 	unsigned char		*d;
+// 	const unsigned char	*s;
+
+// 	// if (!dst && !src)
+// 	// 	return (NULL);
+// 	d = (unsigned char *)dst;
+// 	s = (const unsigned char *)src;
+// 	if (d < s)
+// 		return (ft_memcpy(dst, src, len));
+// 	else
+// 	{
+// 		while (len > 0)
+// 		{
+// 			len--;
+// 			d[len] = s[len];
+// 		}
+// 	}
+// 	return (d);
+// }
+
+// void	*ft_memmove(void *dst, const void *src, size_t n)
+// {
+// 	unsigned char	*d;
+// 	unsigned char	*s;
+
+// 	d = (unsigned char *)dst;
+// 	s = (unsigned char *)src;
+// 	if (d < s)
+// 		return (memcpy(dst, src, n));
+// 	else
+// 	{
+// 		while (n > 0)
+// 		{
+// 			n--;
+// 			d[n] = s[n];
+// 		}
+// 	}
+// 	return (dst);
 // }
