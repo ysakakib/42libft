b448be55600db32eaaa16199a4e2217e0853a76b
Yu Sakakibara <yusakaki@c2r1s1.42tokyo.jp>
2026-05-03 18:48:23 +0900

    A

--- ft_memset.c
+++ ft_memset.c
@@ -5,12 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: yusakaki <yusakaki@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/04/24 16:03:14 by yusakaki          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2026/05/03 11:41:44 by yusakaki         ###   ########.fr       */
-=======
-/*   Updated: 2026/05/03 03:32:57 by yusakaki         ###   ########.fr       */
->>>>>>> 4390aeb (A)
+/*   Created: 2026/05/03 13:41:55 by yusakaki          #+#    #+#             */
+/*   Updated: 2026/05/03 13:42:00 by yusakaki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +14,13 @@
 
 void	*ft_memset(void *b, int c, size_t len)
 {
-	unsigned char *buffer = (unsigned char *)b;
-	unsigned char uc = (unsigned char)c;
-	size_t i = 0;
+	unsigned char	*buffer;
+	unsigned char	uc;
+	size_t			i;
 
+	buffer = (unsigned char *)b;
+	uc = (unsigned char)c;
+	i = 0;
 	while (i < len)
 	{
 		buffer[i] = uc;
--- ft_strchr.c
+++ ft_strchr.c
@@ -6,15 +6,17 @@
 /*   By: yusakaki <yusakaki@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/27 11:15:05 by yusakaki          #+#    #+#             */
-/*   Updated: 2026/04/28 12:17:16 by yusakaki         ###   ########.fr       */
+/*   Updated: 2026/05/03 17:17:05 by yusakaki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_strchr(const char *s, int c)
+char	*ft_strchr(const char *s, int c)
 {
-	char	cc = (char)c;
+	char	cc;
+
+	cc = (char)c;
 	while (*s != '\0')
 	{
 		if (*s == cc)
